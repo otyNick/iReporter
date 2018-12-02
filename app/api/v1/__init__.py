86136f54b712f0bc79da20039f8e1e11eef4ceb1
Nick <omondioty@gmail.com>
2018-12-02 19:44:45 +0300

    Fixed endpoint url_prefix in app/v1/__init__.py

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -5,7 +5,7 @@ from flask_restful import Api, Resource
 from app.api.v1.redflags.views import MyIncident, MyIncidents
 
 
-v1 = Blueprint('api-v1', __name__, url_prefix='/api/v1')
+v1 = Blueprint('api-v1', __name__, url_prefix='/api/v1/redflags')
 api = Api(v1)
 
 ## setup API resource routing

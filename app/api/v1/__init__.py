ec898718dd8e3f4291cd425ea3b6e485229bf772
Nick <omondioty@gmail.com>
2018-12-02 19:08:23 +0300

    Fixed path conflicts in modules and packages

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask_restful import Api, Resource 
 
 #local import
-from app.api.v1.redflags.models import MyIncident, MyIncidents
+from app.api.v1.redflags.views import MyIncident, MyIncidents
 
 
 v1 = Blueprint('api-v1', __name__, url_prefix='/api/v1')

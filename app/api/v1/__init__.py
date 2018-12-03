7bf2aa01da9745cea52ab5cd666122d9b53298af
Nick <omondioty@gmail.com>
2018-12-03 13:33:22 +0300

    Fixed path conflict

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -5,7 +5,7 @@ from flask_restful import Api, Resource
 from app.api.v1.redflags.views import MyIncident, MyIncidents
 
 
-v1 = Blueprint('api-v1', __name__, url_prefix='/api/v1/redflags')
+v1 = Blueprint('api-v1', __name__, url_prefix='/api/v1')
 api = Api(v1)
 
 ## setup API resource routing
